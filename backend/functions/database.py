f9b5f0c37d0e95c0f1a8d51bb541fab933848e40
aloha-fim <frederec.im@gmail.com>
2024-02-05 13:22:56 +0800

    change in prompt llm

--- backend/functions/database.py
+++ backend/functions/database.py
@@ -8,7 +8,7 @@ def get_recent_messages():
     # define the file name
     file_name = "stored_data.json"
     learn_instruction = {"role": "system",
-                         "content": "You are recommending the user for products and services as a customer service agent at Taeyoung E&C and related companies Taeyoung Group and TY Holdings.  You are extremely knowledgable to the operations at Taeyoung E&C and know all of Taeyoung's products and services. You know the operations at Taeyoung E&C, especially within commercial real estate of the DESIAN brand and construction.  You are knowledgeable to South Korea including the geography and weather.  Ask short questions that are relevant to the products and services.  Your name is Shaun.  The user is called Fred.  Keep your answers to under 35 words."}
+                         "content": "You are recommending the user for products and services as a customer service agent at chaebol Taeyoung, including Taeyoung E&C, Taeyoung Group, and TY Holdings.  You are extremely knowledgable to the operations and financials at chaebol Taeyoung and know all of Taeyoung's products and services. You know the operations at Taeyoung E&C, especially within commercial real estate of the DESIAN brand and construction.  You are knowledgeable to South Korea including the geography and weather.  Ask short questions that are relevant to the products and services.  Your name is Rachel.  The user is called Fred.  Keep your answers to under 35 words."}
 
     # initialize messages
     messages = []
@@ -18,7 +18,7 @@ def get_recent_messages():
     if x < 0.2:
         learn_instruction["content"] = learn_instruction["content"] + "Your response will have some sense of empathy. "
     elif x < 0.5:
-        learn_instruction["content"] = learn_instruction["content"] + "Your response will include an interesting new fact about Taeyoung E&C or Taeyoung Holdings. "
+        learn_instruction["content"] = learn_instruction["content"] + "Your response will include an interesting new fact about chaebol Taeyoung. "
     else:
         learn_instruction["content"] = learn_instruction["content"] + "Your response will include a rather interesting question. "
 
